ea7e884ebe8765058403ebfa542a11733b79304a
ddavis60 <ddavis60@ggc.edu>
2020-10-17 16:33:02 -0400

    added menu bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './App.css';
 import DashboardScreen from './screens/DashboardScreen';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component{
     render() {
--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Navbar, Button} from "react-bootstrap";
+import Logo from "../resources/Logo.png"
 
 function DashboardScreen() {
     return (
-        <h1>MAIN PAGE</h1>
+        <container>
+            <Navbar bg="dark">
+                <Navbar.Brand>
+                    <img
+                        src={Logo}
+                        width="75f"
+                        height="50f"
+                        className ="d-inline-block align top"
+                        alt ="Sequence, Oscillate, n GO!"
+                    />
+                </Navbar.Brand>
+                <Button></Button>
+            </Navbar>
+            <h1>MAIN PAGE</h1>
+        </container>
     );
 }
 
